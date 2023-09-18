4b3aa993ccd178f5fd622339dc3ea125b5a4a7d6
Mawunyo Hafoba <mawunyohafoba28@gmail.com>
2023-09-18 16:46:04 +0100

    Added subtract function

--- Ep002/source/main.cpp
+++ Ep002/source/main.cpp
@@ -8,9 +8,17 @@ T add(T a, T b)
     return a + b;
 }
 
+template <typename A>
+requires std::integral<A>
+A subtract(A a, A b)
+{
+    return a - b;
+}
+
 int main()
 {
     std::cout << "Hello from C++ 20 with CMake on Linux/Windows" << std::endl;
     std::cout << "The sum is : " << add(7, 5) << std::endl;
+    std::cout << "The difference is : " << subtract(7, 5) << std::endl;
     return 0;
 }
