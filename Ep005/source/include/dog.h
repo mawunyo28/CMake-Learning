03e73d0da9b0f473b26a7bd4aa3ad1993ef00c8e
Mawunyo Hafoba <mawunyohafoba28@gmail.com>
2023-09-20 16:02:54 +0000

    Fifth Episode

---
+++ Ep005/source/include/dog.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+class Dog
+{
+public:
+    explicit Dog(std::string name_param);
+    Dog() = delete;
+    ~Dog();
+
+    std::string get_name() const
+    {
+        return dog_name;
+    }
+
+    void set_dog_name(const std::string &name)
+    {
+        dog_name = name;
+    }
+
+    void print_info()
+    {
+        std::cout << "Dog [ name : " << dog_name << " ]" << std::endl;
+    }
+
+private:
+    std::string dog_name {"Puffy"};
+};
